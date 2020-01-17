0cc62056e3fc9cbd35dcd807e2ca87de3cb3a322
Jon Schlipf <jon.schlipf@b-tu.de>
2020-01-17 15:08:33 +0100

    added function to make a drawable representation of the geometry

--- src/models.jl
+++ src/models.jl
@@ -4,13 +4,17 @@ using ..materials
 using ..ft2d
 
 export Custom,Layer,PlainLayer,PatternedLayer,Model,Circle,Rectangle,Ellipse,reciprocal
-export Combination,Rotation,Shift
+export Combination,Rotation,Shift,drawable
 abstract type Geometry end
 struct Circle <: Geometry
     radius::Float64
 end
 function reciprocal(c::Circle,dnx,dny)
     return circft(c.radius,dnx,dny)
+end
+function drawable(c::Circle)
+    phivals=2pi*(0:.01:1)
+    return c.radius*cos.(phivals),c.radius*sin.(phivals)
 end
 struct Rectangle <: Geometry
     dx::Float64
@@ -19,11 +23,17 @@ end
 function reciprocal(r::Rectangle,dnx,dny)
     return rectft(r.dx,r.dy,dnx,dny)
 end
+function drawable(r::Rectangle)
+    return [.5*r.dx,-.5*r.dx,-.5*r.dx,.5*r.dx],[.5*r.dy,.5*r.dy,-.5*r.dy,-.5*r.dy]
+end
 struct Custom <: Geometry
     F::Array{Complex{Float64},2}
 end
 function reciprocal(c::Custom,dnx,dny)
     return c.F
+end
+function drawable(c::Custom)
+    return 0,0
 end
 struct Rotation <: Geometry
     G::Geometry
@@ -34,6 +44,12 @@ function reciprocal(c::Rotation,dnx,dny)
     dny2=-dnx*sin(c.θ)+dny*cos(c.θ)
     return reciprocal(c.G,dnx2,dny2)
 end
+function drawable(c::Rotation)
+    x,y=drawable(c.G)
+    u=x*cos(c.θ)+y*sin(c.θ)
+    v=-x*sin(c.θ)+y*cos(c.θ)
+    return u,v
+end
 struct Shift <: Geometry
     G::Geometry
     ax::Float64
@@ -41,6 +57,10 @@ struct Shift <: Geometry
 end
 function reciprocal(c::Shift,dnx,dny)
     return reciprocal(c.G,dnx,dny).*exp.(-2im*pi*c.ax.*dnx).*exp.(-2im*pi*c.ay.*dny)
+end
+function drawable(c::Shift)
+    x,y=drawable(c.G)
+    return x+c.ax,y+c.ay
 end
 struct Combination <: Geometry
     G::Array{Geometry,1}
@@ -52,7 +72,15 @@ function reciprocal(c::Combination,dnx,dny)
     end
     return ret
 end
-
+function drawable(c::Combination)
+    x,y=drawable(c.G[1])
+    for i=2:length(c.G)
+        xon,yon=drawable(c.G[i])
+        x=cat(x,xon,dims=1)
+        y=cat(y,yon,dims=1)
+    end
+    return x,y
+end
 struct Ellipse <: Geometry
     rx::Float64
     ry::Float64
@@ -60,6 +88,12 @@ end
 function reciprocal(e::Ellipse,dnx,dny)
     return ellipft(e.rx,e.ry,dnx,dny)
 end
+
+function drawable(c::Ellipse)
+    phivals=2pi*(0:.01:1)
+    return c.rx*cos.(phivals),c.ry*sin.(phivals)
+end
+
 struct arbitrary <: Geometry
     mask::Array{Float64,2}
 end
