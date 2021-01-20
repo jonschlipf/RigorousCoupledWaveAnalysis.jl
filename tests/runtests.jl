021aeab0db2f3f28a8b05172a085d094a8ce4a59
Jon Schlipf <jon.schlipf@b-tu.de>
2021-01-20 17:11:26 +0100

    added tests

---
+++ tests/runtests.jl
@@ -0,0 +1,12 @@
+using RCWA,Test
+@testset "VerticalIncidence" begin
+n1=rand()*10
+n2=rand()*10
+
+mdl=RCWAModel([],ConstantPerm(n1),ConstantPerm(n2))
+grd=rcwagrid(0,0,100,100,1E-3,rand()*360,1000)
+ste,stm=rwasource(grd,n1)
+R,T=etm_reftra(ste,mdl,grd,1000)
+@test abs(((n1-n2)/(n1+n2))^2-R)<1E-6
+@test abs(1-((n1-n2)/(n1+n2))^2-T)<1E-6
+end
