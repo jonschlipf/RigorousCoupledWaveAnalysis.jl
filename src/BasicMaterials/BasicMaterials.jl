7cd7f116e0f6759ed2cc36402913c8fa56c3547f
Jón Schlipf <schlipf.jon@gmx.net>
2021-02-16 13:36:25 +0100

    export si3n4

--- src/BasicMaterials/BasicMaterials.jl
+++ src/BasicMaterials/BasicMaterials.jl
@@ -1,6 +1,6 @@
 module BasicMaterials
 
-export ge_nunley,si_schinke,sio2_malitson,zno_bond,al_rakic
+export ge_nunley,si_schinke,sio2_malitson,zno_bond,al_rakic,si3n4_luke
 
 using CSV,Interpolations,DataFrames
 include("rakic.jl")
