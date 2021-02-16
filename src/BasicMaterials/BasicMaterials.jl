93c5f4c5e667ddd24a0b3a6b8eab4ae95aeef0cb
Jón Schlipf <schlipf.jon@gmx.net>
2021-02-16 12:10:58 +0100

    added Si3N4 refractive index

--- src/BasicMaterials/BasicMaterials.jl
+++ src/BasicMaterials/BasicMaterials.jl
@@ -14,6 +14,10 @@ sio2_malitson_n(l)=sqrt(1+(0.6961663l^2)/(l^2-0.0684043^2)+(0.4079426l^2)/(l^2-0
 sio2_malitson(l)=sio2_malitson_n(.001l)^2 .+0im
 #I. H. Malitson. Interspecimen comparison of the refractive index of fused silica, J. Opt. Soc. Am. 55, 1205-1208 (1965)
 
+si3n4_luke_n(l)=sqrt(1+(3.0249l^2)/(l^2-0.1353406^2)+(40314l^2)/(l^2-1239.842^2))
+si3n4_luke(l)=si3n4_luke_n(.001l)^2 .+0im
+#K. Luke, Y. Okawachi, M. R. E. Lamont, A. L. Gaeta, M. Lipson. Broadband mid-infrared frequency comb generation in a Si3N4 microresonator, Opt. Lett. 40, 4823-4826 (2015)
+
 zno_bond_n(l)=sqrt(2.81418+0.87968l^2 /(l^2-0.3042^2)-0.00711.*l.^2)
 zno_bond(l)=zno_bond_n(.001l)^2 .+0im
 #W. L. Bond. Measurement of the refractive indices of several crystals, J. Appl. Phys. 36, 1674-1677 (1965)
