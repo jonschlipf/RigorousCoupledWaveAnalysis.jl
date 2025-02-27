c91fd546261c0ba1b197d8f1c1e0aa9551ef5076
Jon Schlipf <schlipf@ihp-microelectronics.com>
2025-02-27 16:55:39 +0100

    supporting simple and anisotropic layers in taylor as well

--- src/Taylor/Taylor.jl
+++ src/Taylor/Taylor.jl
@@ -3,6 +3,72 @@ using LinearAlgebra,CUDA
 using ..Common
 
 export squarescale_reftra
+taylorx(dnx,dny,Kx,Ky,λ,l::SimpleLayer)=eigenmodes(dnx,dny,Kx,Ky,λ,l).X
+function taylorx(dnx,dny,Kx,Ky,λ,l::AnisotropicLayer)
+    # probably doable without taylor as well?
+    IMa=Diagonal(Kx*0 .+1)
+    IM=[IMa 0*IMa;0*IMa IMa]
+    k0=2π/real(λ)
+    #get the base permittivity
+    εxx=get_permittivity(l.materials[1],λ,1)*I
+    εxy=get_permittivity(l.materials[1],λ,2)*I
+    εyx=get_permittivity(l.materials[1],λ,3)*I
+    εyy=get_permittivity(l.materials[1],λ,4)*I
+    εzz=get_permittivity(l.materials[1],λ,5)*I
+    a01=0
+    a11=-0.10036558103014462001
+    a21=-0.00802924648241156960
+    a31=-0.00089213849804572995
+
+    b01=0
+    b11=0.39784974949964507614
+    b21=1.36783778460411719922
+    b31=0.49828962252538267755
+    b41=-0.00063789819459472330
+
+    b02=-10.9676396052962062593
+    b12=1.68015813878906197182
+    b22=0.05717798464788655127
+    b32=-0.00698210122488052084
+    b42=0.00003349750170860705
+
+    b03=-0.09043168323908105619
+    b13=-0.06764045190713819075
+    b23=0.06759613017704596460
+    b33=0.02955525704293155274
+    b43=-0.00001391802575160607
+
+    b04=0
+    b14=0
+    b24=-0.09233646193671185927
+    b34=-.01693649390020817171
+    b44=-0.00001400867981820361
+    η=inv(εzz)
+    P=[Kx*η*Ky I-Kx*η*Kx;Ky*η*Ky-I -Ky*η*Kx]
+    Q=[Kx*Ky+εyx εyy-Kx*Kx;Ky*Ky-εxx -εxy-Ky*Kx]
+    A0=[0IM P;Q 0IM]*k0*l.thickness
+    nrm=maximum(sum(abs.(A0),dims=1))
+    m=Int(ceil(log2(nrm)))
+    A=A0*2.0^-m
+    PQ=P*Q
+    PQP=PQ*P
+    QP=Q*P
+    QPQ=QP*Q
+    A2=[PQ 0IM;0IM QP]*(k0*l.thickness*2.0^-m)^2
+    A3=[0IM PQP;QP*Q 0IM]*(k0*l.thickness*2.0^-m)^3
+    A6=[PQP*QPQ 0IM;0IM QPQ*PQP]*(k0*l.thickness*2.0^-m)^6
+    #A2=A*A
+    #A3=A2*A
+    #A6=A3*A3
+    B1=a01*I+a11*A+a21*A2+a31*A3
+    B2=b01*I+b11*A+b21*A2+b31*A3+b41*A6
+    B3=b02*I+b12*A+b22*A2+b32*A3+b42*A6
+    B4=b03*I+b13*A+b23*A2+b33*A3+b43*A6
+    B5=b04*I+b14*A+b24*A2+b34*A3+b44*A6
+    A9=B1*B5+B4
+    X=B2+(B3+A9)*A9
+    return X^(2^m)
+end
 
 function taylorx(dnx,dny,Kx,Ky,λ,l::PatternedLayer)
     IMa=Diagonal(Kx*0 .+1)
